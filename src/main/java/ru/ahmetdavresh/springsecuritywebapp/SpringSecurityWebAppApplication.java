7c950f0baa66387a5c59ec9a565b919c56032054
ahmet <ahmetdavresh@gmail.com>
2023-11-19 23:56:38 +0300

    init project

---
+++ src/main/java/ru/ahmetdavresh/springsecuritywebapp/SpringSecurityWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ahmetdavresh.springsecuritywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ahmetdavresh/springsecuritywebapp/SpringSecurityWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ahmetdavresh.springsecuritywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
